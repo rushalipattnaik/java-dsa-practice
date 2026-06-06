6857081a3405b8ae133fe4a6b501f83d69e2864c
Rushali Pattnaik <pattnaikrushali@gmail.com>
2026-06-06 23:05:52 +0530

    Implement sorting algorithm: Quick sort.

---
+++ sorting_algorithms/QuickSort.java
@@ -0,0 +1,46 @@
+package sorting_algorithms;
+import java.util.Scanner;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        quickSort(arr, 0, n - 1);
+
+        System.out.print("Sorted Array: ");
+        for (int num : arr) System.out.print(num + " ");
+        System.out.println();
+        sc.close();
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+}
