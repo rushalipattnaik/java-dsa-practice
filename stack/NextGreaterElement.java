c01f688e7e611fa90594b8b5b25e27e14f3857a6
Rushali Pattnaik <pattnaikrushali@gmail.com>
2026-06-12 20:24:38 +0530

    Implement stack problem

---
+++ stack/NextGreaterElement.java
@@ -0,0 +1,20 @@
+package stack;
+import java.util.*;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() <= arr[i]) st.pop();
+            res[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(arr[i]);
+        }
+        System.out.println(Arrays.toString(res));
+        sc.close();
+    }
+}
