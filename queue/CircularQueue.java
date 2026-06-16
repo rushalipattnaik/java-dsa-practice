c1c28e899d5749ea3114c891e025d291c986d084
Rushali Pattnaik <pattnaikrushali@gmail.com>
2026-06-16 16:49:53 +0530

    Implement queue problem

---
+++ queue/CircularQueue.java
@@ -0,0 +1,33 @@
+package queue;
+import java.util.Scanner;
+
+public class CircularQueue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter capacity: ");
+        int n = sc.nextInt();
+        int[] queue = new int[n];
+        int front = 0, rear = 0, size = 0;
+
+        while (true) {
+            System.out.println("1.Enqueue 2.Dequeue 3.Exit");
+            int choice = sc.nextInt();
+            if (choice == 1) {
+                if (size == n) System.out.println("Overflow");
+                else {
+                    queue[rear] = sc.nextInt();
+                    rear = (rear + 1) % n;
+                    size++;
+                }
+            } else if (choice == 2) {
+                if (size == 0) System.out.println("Underflow");
+                else {
+                    System.out.println(queue[front]);
+                    front = (front + 1) % n;
+                    size--;
+                }
+            } else break;
+        }
+        sc.close();
+    }
+}
